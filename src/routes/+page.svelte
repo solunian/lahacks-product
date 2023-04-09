fdfb8c43d8f6561388ec23c858f4b691d34e7799
rk097 <78620631+rk097@users.noreply.github.com>
2023-04-08 17:11:37 -0700

    pilot db

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -9,7 +9,7 @@
         <b>MedTalk</b>
     </h1>
     <h4 class="text-white text-xl">
-      The definite solution for understanding your medication.
+      The definitive solution for understanding your medication.
     </h4>
     <a href="/chat">
       <button class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded-full mt-10 duration-100">
