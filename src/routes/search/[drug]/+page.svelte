2ccbd1f5445fca2ef3782cd748e2e15b0421dbb8
Daniel Cui <solunian@gmail.com>
2023-04-08 21:33:29 -0700

    dynamic route pain in [drug]

---
+++ src/routes/search/[drug]/+page.svelte
@@ -0,0 +1,23 @@
+<script>
+	export let data;
+</script>
+
+<div class="flex flex-col whitespace-normal w-3/4 gap-3">
+	<h1>{data.name}</h1>
+
+	<div>
+		{data.med["other_names"]}
+	</div>
+
+	<div>
+		{data.med["use"]}
+	</div>
+
+	<div>
+		{data.med["side_effects"]}
+	</div>
+
+	<div>
+		{data.med["warnings"]}
+	</div>
+</div>
