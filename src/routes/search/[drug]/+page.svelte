4ef7d8c26668cb43639fe6d513fe6d423dbb4bb3
Daniel Cui <solunian@gmail.com>
2023-04-09 06:39:10 -0700

    drug pages ui finished

--- src/routes/search/[drug]/+page.svelte
+++ src/routes/search/[drug]/+page.svelte
@@ -2,22 +2,36 @@
 	export let data;
 </script>
 
-<div class="flex flex-col whitespace-normal w-3/4 gap-3">
-	<h1>{data.name}</h1>
+<div id="wrapper" class="flex flex-col whitespace-normal gap-4 rounded-lg p-5 border bg-teal-50 mt-16">
+	
+	<h1 class="text-4xl">{data.name}</h1>
 
-	<div>
-		{data.med["other_names"]}
+	<div class="flex flex-row gap-2">
+		{#each data.med["other_names"].split(", ") as name}
+			<div class="border bg-teal-100 rounded-lg p-1 px-2">
+				{name}
+			</div>
+		{/each}
 	</div>
 
 	<div>
+		<h2 class="text-xl">Uses:</h2>
 		{data.med["use"]}
 	</div>
 
 	<div>
+		<h2 class="text-xl">Side Effects:</h2>
 		{data.med["side_effects"]}
 	</div>
 
 	<div>
+		<h2 class="text-xl">Warnings:</h2>
 		{data.med["warnings"]}
 	</div>
 </div>
+
+<style>
+	#wrapper {
+		width: 40rem;
+	}
+</style>
