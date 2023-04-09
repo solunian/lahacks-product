754c2d577e3feb8aff51cc2e68c797173f68a320
rk097 <78620631+rk097@users.noreply.github.com>
2023-04-08 20:34:35 -0700

    about

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -2,7 +2,7 @@
   import bg from "$lib/images/bg.jpg";
 </script>
 
-<div id="body" style="background-image: url({bg}); height: 1000px;" class="relative overflow-hidden p-12 text-center">
+<div id="body" style="background-image: url({bg}); height: 800px;" class="relative overflow-hidden p-12 text-center">
   <div id="darken"class="absolute bottom-0 left-0 right-0 top-0 h-full w-full overflow-hidden bg-fixed"
   style="background-color: rgba(0, 0, 0, 0.2)">
     <h1 class="text-7xl text-white" style="margin-top: 10%;">
@@ -16,6 +16,14 @@
         {"Start your MedTalk journey →"}
       </button>
     </a>
+    <div class="shadow-xl flex flex-row rounded-xl bg-slate-200 bg-clip-border" style="margin-top: 5%; margin-left: auto; margin-right: auto; width: 80%;">
+      <div class="flex-1 p-6">
+        <h2 class="text-xl"><b>About MedTalk</b></h2>
+        <p class="opacity-60 mb-3 text-lg mt-2">
+          It is common knowledge that medicine is a complex subject matter. But it shouldn't be--we're putting these pills and liquids into our bodies after all! By integrating the rising paradigm of language models along with pharmaceutical data, with a sentence and a click, we can bring you everything you need to treat your condition and things to know about your medicine.
+        </p>
+      </div>
+    </div>
   </div>
 </div>
 
